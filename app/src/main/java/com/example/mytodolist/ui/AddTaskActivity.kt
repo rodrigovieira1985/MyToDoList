079ba45dfd3eca5a74c35640ec4e879f8f3180d1
rodrigovieira1985 <rodrigovs@hotmail.com>
2021-08-30 11:41:40 -0300

    add icone +

---
+++ app/src/main/java/com/example/mytodolist/ui/AddTaskActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mytodolist.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddTaskActivity : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        ActivityAddTaskBinding
+
+    }
+
+
+}
